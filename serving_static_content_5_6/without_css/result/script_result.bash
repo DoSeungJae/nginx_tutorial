e41194125d0eca8b2a029a43d85b9b713f08b399
DSJ <95843713+DoSeungJae@users.noreply.github.com>
2025-04-30 13:51:12 +0000

    how to serve static content : without css and with css

---
+++ serving_static_content_5_6/without_css/result/script_result.bash
@@ -0,0 +1,21 @@
+root@a533debaae73:/# curl -I http://nginx-handbook.test:801 
+HTTP/1.1 200 OK
+Server: nginx/1.27.5
+Date: Wed, 30 Apr 2025 13:50:09 GMT
+Content-Type: text/html
+Content-Length: 960
+Last-Modified: Tue, 29 Apr 2025 10:52:18 GMT
+Connection: keep-alive
+ETag: "6810af62-3c0"
+Accept-Ranges: bytes
+
+root@a533debaae73:/# curl -I http://nginx-handbook.test:801/mini.min.css
+HTTP/1.1 200 OK
+Server: nginx/1.27.5
+Date: Wed, 30 Apr 2025 13:50:14 GMT
+Content-Type: text/plain ##!!
+Content-Length: 46887
+Last-Modified: Tue, 29 Apr 2025 10:52:18 GMT
+Connection: keep-alive
+ETag: "6810af62-b727"
+Accept-Ranges: bytes
